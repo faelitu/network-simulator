29e752a212b8d272231f5a759facab2634584d72
Rafael Machado <rafafsmachado@gmail.com>
2020-08-06 17:06:57 -0300

    aula 1 OK

--- simulator.py
+++ simulator.py
@@ -1,11 +1,20 @@
 import random as rd
+import math
 
 def aleatorio():
-    rd.seed(a=55) # DEVELOPMENT
-    #rd.seed() # PRODUCTION
-
+    rd.seed()
     u = rd.random() # Generate a number between [0.0, 1.0)
     u = 1.0 - u # Returns a number between (0.0, 1.0]
     return u
 
-print(aleatorio())
+# MAIN:
+l = float(input("Informe o valor medio: ")) # l == exponencial parameter.
+l = 1.0 / l                                 # E[X] == 1/l
+
+soma = 0.0
+qtd = 10000
+for cont in range(0, qtd):
+    valor = float((-1.0/l) * math.log(aleatorio()))
+    print("Valor gerado: " + str(valor))
+    soma += valor
+print("Media: " + str(soma/qtd))
